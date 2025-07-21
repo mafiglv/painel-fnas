e5e14fb44806a48af49feb09c0be94895e5636b7
Germária Lins <125930920+mafiglv@users.noreply.github.com>
2025-07-21 11:02:30 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           Painel geral de inscrições - FNAS pelo Brasil
         </h1>
         <p className="fst-italic text-update">
-          Atualizado 17/07/2025 às 17:00
+          Atualizado 21/07/2025 às 11:00
         </p>
       </header>
 
